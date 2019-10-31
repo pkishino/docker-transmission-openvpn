f5044e97bccbf010b3e5779cdbdf921ee4f51b69
Patrick Kishino <patrick.a.kishino@gmail.com>
2019-10-31 12:54:26 +0900

    Revert "updated"

--- openvpn/pia/updateConfigs.sh
+++ openvpn/pia/updateConfigs.sh
@@ -8,42 +8,17 @@ cd "${0%/*}"
 # Delete everything (not this script though)
 find . ! -name '*.sh' -delete
 
-baseURL="https://www.privateinternetaccess.com/openvpn/openvpn"
-extension=".zip"
-declare -a configsURLs=(    "" "-strong" "-tcp" "-strong-tcp" "-ip")
-declare -a configsFolders=( "" "strong"  "tcp"  "tcp-strong"  "ip")
+# Get updated configuration zip
+curl -kL https://www.privateinternetaccess.com/openvpn/openvpn.zip -o openvpn.zip \
+  && unzip -j openvpn.zip && rm openvpn.zip
 
-# warning: keeping folder name "tcp-strong" for legacy reasons, but the url is "strong-tcp".
+# Ensure linux line endings
+dos2unix *
 
-numberOfConfigTypes=${#configsURLs[@]}
-
-for (( i=1; i<${numberOfConfigTypes}+1; i++ ));
-do
-  requestURL="$baseURL${configsURLs[$i-1]}$extension"
-  if [ ! -z "${configsFolders[$i-1]}" ]
-  then
-    mkdir -p ${configsFolders[$i-1]} && cd ${configsFolders[$i-1]}
-  fi
-  curl -kL $requestURL -o openvpn.zip \
-    && unzip -j openvpn.zip && rm openvpn.zip
-
-  # Ensure linux line endings
-  dos2unix *
-
-  # Update configs with correct paths
-  folderNameWithEscapedSlash=""
-  if [ ! -z "${configsFolders[$i-1]}" ]
-  then
-    folderNameWithEscapedSlash="${configsFolders[$i-1]}\/"
-  fi
-  sed -i "s/auth-user-pass/auth-user-pass \/config\/openvpn-credentials.txt/" *.ovpn
-  sed -i "s/ca ca\.rsa\.\([0-9]*\)\.crt/ca \/etc\/openvpn\/pia\/${folderNameWithEscapedSlash}ca\.rsa\.\1\.crt/" *.ovpn
-  sed -i "s/crl-verify crl\.rsa\.\([0-9]*\)\.pem/crl-verify \/etc\/openvpn\/pia\/${folderNameWithEscapedSlash}crl\.rsa\.\1\.pem/" *.ovpn
-  if [ ! -z "${configsFolders[$i-1]}" ]
-  then
-    cd ..
-  fi
-done
+# Update configs with correct paths
+sed -i "s/ca ca\.rsa\.2048\.crt/ca \/etc\/openvpn\/pia\/ca\.rsa\.2048\.crt/" *.ovpn
+sed -i "s/crl-verify crl\.rsa\.2048\.pem/crl-verify \/etc\/openvpn\/pia\/crl\.rsa\.2048\.pem/" *.ovpn
+sed -i "s/auth-user-pass/auth-user-pass \/config\/openvpn-credentials.txt/" *.ovpn
 
 # Create symlink for default.ovpn
-ln -s "CA Toronto.ovpn" default.ovpn
+ln -s Netherlands.ovpn default.ovpn
--- openvpn/torguard/updateConfigs.sh
+++ openvpn/torguard/updateConfigs.sh
@@ -14,18 +14,12 @@ curl -L https://torguard.net/downloads/OpenVPN-UDP.zip -o OpenVPN-UDP.zip \
 
 # Remove TorGuard prefix of config files and ensure linux line endings
 rename 's/^TorGuard\.//' *.ovpn
-
-# Convert CRLF to Linux
-sed -i "s/\r$//" *.ovpn
+dos2unix *
 
 # Update configs with correct paths
 sed -i "s/ca ca.crt/ca \/etc\/openvpn\/torguard\/ca.crt/" *.ovpn
 sed -i "s/tls-auth ta.key/tls-auth \/etc\/openvpn\/torguard\/ta.key/" *.ovpn
 sed -i "s/auth-user-pass/auth-user-pass \/config\/openvpn-credentials.txt/" *.ovpn
-
-# Remove incompatible directives
-sed -i "s/^block-outside-dns/# block-outside-dns/" *.ovpn
-sed -i "s/^keepalive/# keepalive/" *.ovpn
 
 # Create symlink for default.ovpn
 ln -s Netherlands.ovpn default.ovpn
